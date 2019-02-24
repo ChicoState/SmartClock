db8f61006cc9a8b2ecd3ed2f81cb26dfe24dfcae
1miqbal1 <miqbal1@mail.csuchico.edu>
2019-02-23 22:19:38 -0800

    added sources

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# adapted from https://stackoverflow.com/questions/18923321/making-a-clock-in-kivy
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.graphics import Color, Line
