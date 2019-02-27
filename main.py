ebea1fcde6fefaa8dbf60a75ce9062fe11c6fcff
1miqbal1 <miqbal1@mail.csuchico.edu>
2019-02-26 17:11:19 -0800

    fixed clock ticks

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # adapted from https://stackoverflow.com/questions/18923321/making-a-clock-in-kivy
+# adapted from https://github.com/akrog100/Meza/blob/master/main.py
 
 from kivy.app import App
 import json
@@ -102,17 +103,19 @@ class Ticks(Widget):
         super(Ticks, self).__init__(**kwargs)
         self.bind(pos=self.update_clock)
         self.bind(size=self.update_clock)
+        Clock.schedule_interval(self.update_clock, 1)
+
 
     def update_clock(self, *args):
         self.canvas.clear()
         with self.canvas:
-            time = datetime.datetime.now()
+            clocktime = datetime.datetime.now()
             Color(0.2, 0.5, 0.2)
-            Line(points=[self.center_x, self.center_y, self.center_x+0.8*self.r*sin(pi/30*time.second), self.center_y+0.8*self.r*cos(pi/30*time.second)], width=1, cap="round")
+            Line(points=[self.center_x, self.center_y, self.center_x+0.8*self.r*sin(pi/30*clocktime.second), self.center_y+0.8*self.r*cos(pi/30*clocktime.second)], width=1, cap="round")
             Color(0.3, 0.6, 0.3)
-            Line(points=[self.center_x, self.center_y, self.center_x+0.7*self.r*sin(pi/30*time.minute), self.center_y+0.7*self.r*cos(pi/30*time.minute)], width=2, cap="round")
+            Line(points=[self.center_x, self.center_y, self.center_x+0.7*self.r*sin(pi/30*clocktime.minute), self.center_y+0.7*self.r*cos(pi/30*clocktime.minute)], width=2, cap="round")
             Color(0.4, 0.7, 0.4)
-            th = time.hour*60 + time.minute
+            th = clocktime.hour*60 + clocktime.minute
             Line(points=[self.center_x, self.center_y, self.center_x+0.5*self.r*sin(pi/360*th), self.center_y+0.5*self.r*cos(pi/360*th)], width=3, cap="round")
 
 class PopupDismissButton(Button):
@@ -226,9 +229,9 @@ sm.add_widget(HomeScreen(name='home'))
 sm.add_widget(AlarmScreen(name='alarm'))
 class MyClockApp(App):
     def build(self):
-        clock = HomeScreen()
-        Clock.schedule_interval(clock.ticks.update_clock, 1)
+
         return sm
+
 
 if __name__ == '__main__':
     MyClockApp().run()
