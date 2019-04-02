38f70adea72a7aa04e1b59bb148555da9f712d05
Matthew Hardenburgh <mdhardenburgh@gmail.com>
2019-04-01 21:55:39 -0700

    Added the color change functionality

--- main.py
+++ main.py
@@ -24,12 +24,16 @@ import time
 import datetime
 import subprocess
 import os
-#from lights import RGB
-#from lights import Lights
+# from lights import RGB
+# from lights import Lights
 
 Builder.load_file('alarmClock.kv');
 Builder.load_file('alarmScreen.kv');
 Builder.load_file('lights.kv');
+
+# dayColor = RGB(255, 255, 255) #Create a day RGB object (default color white light)
+# nightColor = RGB(255, 0, 0) #Create a night RGB object (default color red light)
+# myLights = Lights(dayColor, 0.1, 0, 0) #Create a light object
 
 alarm_hour = 0;
 alarm_minute = 0
@@ -98,32 +102,84 @@ class Ticks(Widget):
         local_minute = int(now.minute)
         global wait_next_minute
 
+        # redDistance = dayColor.getRed() - nightColor.getRed() #final - initial
+        # greenDistance = dayColor.getGreen() - nightColor.getGreen() #final - inital
+        # blueDistance = dayColor.getBlue() - nightColor.getBlue() #final - initial
+        # red = 0
+        # green = 0
+        # blue = 0
+
         #logic to ensure alarm function only fires once when it is the alarm time
         if(wait_next_minute!=0 and local_minute!=alarm_minute):
             wait_next_minute = 0
 
-        elif(now > alarmDeltaThirty):
+        if(now > alarmDeltaThirty):
             pass
-            #sleep to wake
-            #implement changing colors here
-            #elif()#wake to sleep
+            # if(redDistance > 0):
+            #     red = nightColor + (redDistance/30)
+            # if(redDistance < 0):
+            #     red = nightColor - (redDistance/30)
+
+            # if(greenDistance > 0):
+            #     green = nightColor + (greenDistance/30)
+            # if(redDistance < 0):
+            #     green = nightColor - (greenDistance/30)
+
+            # if(greenDistance > 0):
+            #     blue = nightColor + (blueDistance/30)
+            # if(redDistance < 0):
+            #     blue = nightColor - (blueDistance/30)
+
+            # myLights.setRGB(red, green, blue)
 
         elif((local_hour == alarm_hour and local_minute == alarm_minute) and wait_next_minute == 0):
             self.alarm_func()
+            # myLights.setColor(dayColor)
             wait_next_minute = 1
 
     def checkSleep(self, *args):
         global sleep_hour
         global sleep_minute
         now = datetime.datetime.now()
+
+        sleepDateTime = datetime.datetime(now.year, now.month, now.day, sleep_hour, sleep_minute, 0)
+        sleepDeltaThirty = sleepDateTime - datetime.timedelta(minutes=30)
         local_shour = int(now.hour)
         local_sminute = int(now.minute)
         global wait_next_sminute
 
+        # redDistance = nightColor.getRed() - dayColor.getRed() #final - initial
+        # greenDistance = nightColor.getGreen() - dayColor.getGreen() #final - inital
+        # blueDistance = nightColor.getBlue() - dayColor.getBlue() #final - initial
+        # red = 0
+        # green = 0
+        # blue = 0
+
         if(wait_next_sminute!=0 and local_sminute!=sleep_minute):
             wait_next_sminute = 0
+
+        if(now > sleepDeltaThirty):
+            pass
+            # if(redDistance > 0):
+            #     red = dayColor + (redDistance/30)
+            # if(redDistance < 0):
+            #     red = dayColor - (redDistance/30)
+
+            # if(greenDistance > 0):
+            #     green = dayColor + (greenDistance/30)
+            # if(redDistance < 0):
+            #     green = dayColor - (greenDistance/30)
+
+            # if(greenDistance > 0):
+            #     blue = dayColor + (blueDistance/30)
+            # if(redDistance < 0):
+            #     blue = dayColor - (blueDistance/30)
+
+            # myLights.setRGB(red, green, blue)
+
         elif((local_shour == sleep_hour and local_sminute == sleep_minute) and wait_next_sminute == 0):
             self.sleep_func()
+            # myLights.setColor(nightColor)
             wait_next_sminute = 1
 
     def alarm_func(self, *args):
@@ -313,7 +369,4 @@ class MyClockApp(App):
         return sm
 
 if __name__ == '__main__':
-    #dayColor = RGB(255, 255, 255) #Create a day RGB object (default color white light)
-    #nightColor = RGB(255, 0, 0) #Create a night RGB object (default color red light)
-    #myLights = Lights(dayColor, 0.1, 0, 0) #Create a light object
     MyClockApp().run()
